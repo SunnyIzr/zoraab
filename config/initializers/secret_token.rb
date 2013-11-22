c310edf443b3843ef9dcba84cd1778512b84ffa2
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-11-22 16:53:01 -0500

    rails boilerplate code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zoraab::Application.config.secret_key_base = '4402f395306f98fe6a9989ae143c9be5be32f675cf82c17234de5d31de73008099f172b752d356ceac3a942f8e9c8f01159932c48e2df10f3f56b11e9250e02a'
