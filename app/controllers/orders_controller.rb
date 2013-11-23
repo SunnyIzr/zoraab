cf454d26b86a6ea388fa8131fb84242f90d6b538
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-11-23 10:56:31 -0500

    added new form for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,12 @@ class OrdersController < ApplicationController
     @order = Order.find(params[:id])
   end
 
+  def new
+    @order = Order.new
+    p "*"*500
+    p params
+    @sub = Sub.find(params[:sub_id])
+  end
+
+
 end
