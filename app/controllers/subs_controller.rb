895793534df39c2757907bbefbf743359869af83
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-11-22 22:39:38 -0500

    when adding a new subscriber, prints out all chargify information to view

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -15,9 +15,10 @@ class SubsController < ApplicationController
 
   def show
     @sub = Sub.find(params[:id])
-    p "*"*500
-    @chargify_data = Chargify::Subscription.find(@sub.cid)
-    @customer = @chargify_data.attributes["customer"].attributes
+    @chargify_data = Chargify::Subscription.find(@sub.cid).attributes
+    @customer = @chargify_data["customer"].attributes
+    @product = @chargify_data["product"].attributes
+    @billing = @chargify_data["credit_card"].attributes
   end
 
   private
