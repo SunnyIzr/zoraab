4ac8413d618e0d18da3597ee2a28e936ad9bd71e
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-11-22 20:22:28 -0500

    created sub resource with only new, create and show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Zoraab::Application.routes.draw do
   root 'welcome#index'
+
+  resources :subs, only: [:new, :create, :show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
