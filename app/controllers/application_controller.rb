906ee631b71228c099701121baf8cd74a164f864
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-11-22 23:34:06 -0500

    adding basic authentication where username and pw is env variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with :email=>"sunny", :password => "thing"
+  http_basic_authenticate_with :name=>ENV['USERNAME'], :password => ENV['PW']
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
