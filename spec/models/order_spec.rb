56a8a415dd3cec6dc6bd0757d3629aa621564880
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-11-23 00:33:52 -0500

    add tests to validate the orders/spec relationship

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 describe Order do
   it {should validate_presence_of (:order_number)}
   it {should have_and_belong_to_many (:products)}
+  it {should belong_to (:sub)}
 end
--- spec/models/sub_spec.rb
+++ spec/models/sub_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 describe Sub do
   it {should validate_presence_of (:cid)}
   it {should have_and_belong_to_many (:prefs)}
+  it {should have_many (:orders)}
 end
