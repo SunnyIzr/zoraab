f980c60c45c56929d4e2230c0540587559836c09
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-11-24 18:53:39 -0500

    changes for heroku

--- app/models/sub.rb
+++ app/models/sub.rb
@@ -10,9 +10,14 @@ class Sub < ActiveRecord::Base
   end
 
   def retrieve_wufoo_prefs
-    p "*"*100
+    p ">"*100
     p WUFOO
-    p WUFOO.forms
+    all_forms=[]
+    WUFOO.forms.each { |form| all_forms << form.details["Hash"]}
+    all_forms.each do |form_id|
+      entry = WUFOO.form(form_id).entries(:sort => 'EntryId DESC', :limit => 100, :system => true, :filters => [['TransactionId','Is_equal_to',cid]] )
+      return entry[0] unless entry.empty?
+    end
     # self.prefs << Wufoo.find_prefs(cid)
   end
 end
