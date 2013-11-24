2ac676bd336bbe6192c2fe6cf7431f9016361848
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-11-24 18:37:46 -0500

    changes for heroku

--- app/models/sub.rb
+++ app/models/sub.rb
@@ -1,7 +1,7 @@
 class Sub < ActiveRecord::Base
   validates_presence_of :cid
   validates_uniqueness_of :cid
-  before_save :retrieve_wufoo_prefs
+  # before_save :retrieve_wufoo_prefs
   has_and_belongs_to_many :prefs
   has_many :orders
 
