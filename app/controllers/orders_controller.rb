ec10b0bfc0891e9a5c123886cdc27f4d96d61250
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-11-23 20:06:39 -0500

    add order#create action along with neccessary products

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,5 +8,24 @@ class OrdersController < ApplicationController
     @sub = Sub.find(params[:sub_id])
   end
 
+  def create
+    @order = Order.new(order_params)
+    @order.products << Product.find_by(sku:'test')
+    if @order.save
+      redirect_to order_path(@order.id)
+    else
+      render text: "FAIL!"
+    end
+  end
+
+  private
+  def order_params
+    params.permit(:sub_id, :order_number)
+  end
+
+  def items_params
+    params.permit(:item1)
+  end
+
 
 end
