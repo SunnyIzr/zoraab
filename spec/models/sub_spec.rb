08b85a5228392acab19f0b2772351009b60a67e2
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-11-24 16:57:24 -0500

    added validation for uqnieness of cid in subs db with test, test is broken tho!

--- spec/models/sub_spec.rb
+++ spec/models/sub_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
 describe Sub do
-  let (:sub) { FactoryGirl.create(:sub)}
-  let (:response) {sub.chargify}
+  it {should validate_uniqueness_of(:cid) }
   it {should validate_presence_of (:cid)}
   it {should have_and_belong_to_many (:prefs)}
   it {should have_many (:orders)}
