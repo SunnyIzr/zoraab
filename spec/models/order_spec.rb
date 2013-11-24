d234d4d2543efb6ede4eaec62a38c80248215efb
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-11-23 20:27:32 -0500

    established an order number methodology for shipstation and financial purposes: * For previous orders user is able to upload legacy order numbers for QB and SS cross-ref * FOr future orders system will autogenerate order numbers for QB, SS and customer purposes * Order number will be a 4 digit number ending with actual id and starting with prefix K

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe Order do
-  it {should validate_presence_of (:order_number)}
   it {should validate_presence_of (:sub_id)}
   it {should have_and_belong_to_many (:products)}
   it {should belong_to (:sub)}
