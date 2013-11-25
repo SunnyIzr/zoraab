108ce8edc3dbf2426ef49f70fbe316b379cdaa52
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-11-24 20:41:41 -0500

    create shopify module which parses api response into readable hash

---
+++ app/helpers/shopify.rb
@@ -0,0 +1,19 @@
+module Shopify
+  extend self
+
+  def data(sku)
+    hash = {}
+    res = ShopifyAPI::Product.find(:all, :params => {'handle' => sku}).first.attributes
+    {
+      sku: sku,
+      title: res['title'],
+      price: res['price'],
+      q: res['variants'][0].attributes['inventory_quantity'],
+      vendor: res['vendor'],
+      tags: res['tags'],
+      pic: res['image'].attributes['src'],
+      publish_date: res['published_at']
+    }
+
+  end
+end
