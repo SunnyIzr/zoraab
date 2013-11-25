d775b58f6ce16f0a2a8a50a561c206d59a5fc006
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-11-25 00:53:36 -0500

    added reduce shopify inventory method to shopify module

--- app/helpers/shopify.rb
+++ app/helpers/shopify.rb
@@ -15,6 +15,11 @@ module Shopify
       small_pic: res['image'].attributes['src'].gsub(/(.jpeg)/,'_small.jpeg'),
       publish_date: res['published_at']
     }
+  end
 
+  def reduce_shopify_inv(sku)
+    res = ShopifyAPI::Product.find(:all, :params => {'handle' => sku}).first
+    res.attributes['variants'][0].attributes['inventory_quantity'] -= 1
+    res.save
   end
 end
