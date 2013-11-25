b9d901bf1f95d94540ebd52530f3ca2f76bb37b2
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-11-25 13:23:26 -0500

    added active boolean column to products since we cant destroy anything!

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131125153347) do
+ActiveRecord::Schema.define(version: 20131125182230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20131125153347) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "q"
+    t.boolean  "active"
   end
 
   create_table "subs", force: true do |t|
