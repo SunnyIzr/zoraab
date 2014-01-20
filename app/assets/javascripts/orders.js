1b3c3b2913c358a29da0b7b354d5c589cd50d7a8
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-01-20 16:49:48 -0500

    Move all Orders MVC JS to its own JS file

---
+++ app/assets/javascripts/orders.js
@@ -0,0 +1,65 @@
+var OrdersController = {
+  init: function() {
+    this.genButton()
+  },
+
+  genButton: function() {
+    $('#generate-button').click(function(event){
+      event.preventDefault();
+      subId = $(this).attr('data-sub-id')
+      OrdersModel.generateKitterRecs(subId)
+      OrdersModel.addFirstRecsToPos(subId)
+      OrdersView.inputFirstRecs(subId)
+      $('.next-arrow').show();
+
+    })
+  }
+
+}
+
+var OrdersModel = {
+
+  displayedPos: {}
+  //this holds what is currently being shown in hash form ex: {subId, [ary of Pos Ids]}
+  ,
+
+  addFirstRecsToPos: function(subId) {
+    inputSize = $('div[data-sub="'+subId+'"] > div > div > input').size()
+    nextPos = -1
+
+    this.displayedPos[subId] = []
+
+    for (var i = 0; i < inputSize; i++) {
+      this.displayedPos[subId].push(i)
+    }
+  },
+
+  generateKitterRecs: function(subId) {
+    path = '/kitter/'+ subId
+    $.getJSON(path,function(response) {
+    })
+  }
+}
+
+var OrdersView = {
+  inputFirstRecs: function(subId) {
+    inputTags = $('div[data-sub="'+subId+'"] > div > div > input')
+    $.each(inputTags, function(index,inputTag) {
+      path = '/next-kitter/' + subId + '/' + OrdersModel.displayedPos[subId][index]
+      $.getJSON(path, function(response) {
+        $(inputTag).val(response.sku)
+        OrdersView.prevImg(subId,index)
+      });
+    })
+  },
+
+  prevImg: function(subId,pos) {
+    sku = $($('div[data-sub="'+subId+'"] > div > div > input')[pos]).val()
+    ajaxLink = '/products/' + sku
+
+    $.getJSON(ajaxLink,function(response) {
+      imgTag = $($('div[data-sub="'+subId+'"] > div > .preview-image')[pos])
+      imgTag.html('<img src='+response['small_pic']+'>')
+    })
+  }
+}
