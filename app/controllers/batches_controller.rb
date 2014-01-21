eb2aa8afc6e7e1a36221764c76c1eacce0e34f54
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-01-21 17:42:16 -0500

    Newbatch view in new batch action

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -11,4 +11,20 @@ class BatchesController < ApplicationController
       @product_data[product.sku] = Shopify.data(product.sku)
     end
   end
+
+  def new
+    @batch = Batch.new
+    @subs = []
+    @orders = []
+    Sub.pull_subs_due(params['days'].to_i).each do |sub|
+      @subs << ChargifyResponse.parse(sub.chargify)
+      @orders << sub.orders.new
+    end
+    p @subs
+  end
+
+  def create
+
+  end
+
 end
