74cd05f7f312b29af423a5a8574a6143b015b3ca
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-01-27 16:14:34 -0500

    moving js code from app.js to main.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,31 +15,3 @@
 //= require foundation
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
-
-$(document).ready(function() {
-    shopifySync();
-    fulfillRecurSubs();
-    OrdersController.init()
-    BatchController.init()
-
-  });
-
-
-function shopifySync() {
-  $('a#shopify_sync').click(function(event) {
-    event.preventDefault();
-    $('.overlay').show();
-    $('#loader').show();
-    window.location.href = '/sync'
-
-  })
-}
-
-function fulfillRecurSubs () {
-  $('.retrieve-recurring-subs').click(function() {
-    $('.overlay').show();
-    $('#loader').show();
-  })
-
-}
---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,29 @@
+
+$(function(){ $(document).foundation(); });
+
+$(document).ready(function() {
+    shopifySync();
+    fulfillRecurSubs();
+    OrdersController.init()
+    BatchController.init()
+
+  });
+
+
+function shopifySync() {
+  $('a#shopify_sync').click(function(event) {
+    event.preventDefault();
+    $('.overlay').show();
+    $('#loader').show();
+    window.location.href = '/sync'
+
+  })
+}
+
+function fulfillRecurSubs () {
+  $('.retrieve-recurring-subs').click(function() {
+    $('.overlay').show();
+    $('#loader').show();
+  })
+
+}
