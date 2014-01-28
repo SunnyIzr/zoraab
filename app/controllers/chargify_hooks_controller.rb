c525eae494321d0cb71aaa9a8116286ea6b52e07
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-01-27 22:18:11 -0500

    converting params to_i

--- app/controllers/chargify_hooks_controller.rb
+++ app/controllers/chargify_hooks_controller.rb
@@ -12,7 +12,9 @@ class ChargifyHooksController < ApplicationController
       os_signup = OutstandingSignup.create(trans_id: params['payload']['transaction']['id'], cid: params['payload']['subscription']['id'], name: params['payload']['subscription']['customer']['last_name'], plan: params['payload']['subscription']['product']['name'], amount: (params['payload']['transaction']['amount_in_cents'].to_i/100))
     else
       Order.pending.each do |order|
-        if order.sub.cid == params['payload']['subscription']['id']
+        p "*"*100
+        p order.sub.cid
+        if order.sub.cid == params['payload']['subscription']['id'].to_i
           order.trans_id = params['payload']['transaction']['id']
           order.save
           return
