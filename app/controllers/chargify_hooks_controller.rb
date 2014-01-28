dab9d7b2a704d56ef10fc34ae22278113d98e3de
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-01-27 22:08:35 -0500

    Check new transaction against pending orders

--- app/controllers/chargify_hooks_controller.rb
+++ app/controllers/chargify_hooks_controller.rb
@@ -11,6 +11,13 @@ class ChargifyHooksController < ApplicationController
     if Sub.find_by(cid: params['payload']['subscription']['id']) == nil
       os_signup = OutstandingSignup.create(trans_id: params['payload']['transaction']['id'], cid: params['payload']['subscription']['id'], name: params['payload']['subscription']['customer']['last_name'], plan: params['payload']['subscription']['product']['name'], amount: (params['payload']['transaction']['amount_in_cents'].to_i/100))
     else
+      Order.pending.each do |order|
+        if order.sub.cid == params['payload']['subscription']['id']
+          order.trans_id = params['payload']['transaction']['id']
+          order.save
+          return
+        end
+      end
       os_ren = OutstandingRenewal.create(trans_id: params['payload']['transaction']['id'], cid: params['payload']['subscription']['id'], name: params['payload']['subscription']['customer']['last_name'], plan: params['payload']['subscription']['product']['name'], amount: (params['payload']['transaction']['amount_in_cents'].to_i/100))
     end
 
