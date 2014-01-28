ca0971588c25131f3861fd5517418b9c77652369
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-01-27 21:15:44 -0500

    created an if clause to regulate where new transactions go

--- app/controllers/chargify_hooks_controller.rb
+++ app/controllers/chargify_hooks_controller.rb
@@ -7,7 +7,13 @@ class ChargifyHooksController < ApplicationController
     end
     p params
     p '?'*5000
-    os_signup = OutstandingSignup.create(trans_id: params['payload']['transaction']['id'], cid: params['payload']['subscription']['id'], name: params['payload']['subscription']['customer']['last_name'], plan: params['payload']['subscription']['product']['name'], amount: (params['payload']['transaction']['amount_in_cents'].to_i/100))
+
+    if Sub.find_by(cid: params['payload']['subscription']['id']) == nil
+      os_signup = OutstandingSignup.create(trans_id: params['payload']['transaction']['id'], cid: params['payload']['subscription']['id'], name: params['payload']['subscription']['customer']['last_name'], plan: params['payload']['subscription']['product']['name'], amount: (params['payload']['transaction']['amount_in_cents'].to_i/100))
+    else
+      os_ren = OutstandingRenewal.create(trans_id: params['payload']['transaction']['id'], cid: params['payload']['subscription']['id'], name: params['payload']['subscription']['customer']['last_name'], plan: params['payload']['subscription']['product']['name'], amount: (params['payload']['transaction']['amount_in_cents'].to_i/100))
+    end
+
   end
 
   private
