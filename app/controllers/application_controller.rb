301d2873a7e47c87e3c318ccd12b9de2b555537b
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-01-27 20:25:11 -0500

    Change protect_from_forgery for null sessions for hooks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@ class ApplicationController < ActionController::Base
   http_basic_authenticate_with :name=>ENV['USERNAME'], :password => ENV['PW']
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 end
