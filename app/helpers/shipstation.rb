b677561058451293bf7873abcd6dcc4d15ce6f45
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-02-03 13:56:40 -0500

    changing back testing

--- app/helpers/shipstation.rb
+++ app/helpers/shipstation.rb
@@ -28,8 +28,8 @@ module Shipstation
     shipstation_order = blank_order
     shipstation_order.OrderDate = order.created_at
     shipstation_order.PayDate = order.created_at
-    # shipstation_order.OrderNumber = order.order_number
-    shipstation_order.OrderNumber = 'TESTING!!!'
+    shipstation_order.OrderNumber = order.order_number
+    # shipstation_order.OrderNumber = 'TESTING!!!'
     shipstation_order.ShipName = order.name
     shipstation_order.ShipStreet1 = order.address
     shipstation_order.ShipStreet2 = order.address2
