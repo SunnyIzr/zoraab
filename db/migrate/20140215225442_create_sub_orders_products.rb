3dad76182681c7f73cd773268224cba061073604
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-02-15 19:18:47 -0500

    removing bad migrations

--- db/migrate/20140215225442_create_sub_orders_products.rb
+++
@@ -1,8 +0,0 @@
-class CreateSubOrdersProducts < ActiveRecord::Migration
-  def change
-    create_table :sub_orders_products do |t|
-      t.belongs_to :sub_order
-      t.belongs_to :product
-    end
-  end
-end
--- db/migrate/20140215230604_drop_sub_orders_products.rb
+++
@@ -1,5 +0,0 @@
-class DropSubOrdersProducts < ActiveRecord::Migration
-  def change
-    drop_table :sub_orders_products
-  end
-end
