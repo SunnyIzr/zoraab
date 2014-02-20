6be5851ca78b961ab086e2e4f6eee7562be33305
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-02-20 18:51:35 -0500

    set po number action

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,13 @@
 class Invoice < ActiveRecord::Base
   has_many :line_items, as: :line_itemable
+  after_save :set_po_number
+
+  def set_po_number
+    unless self.po_number
+      digits = 4 - self.id.to_s.length
+      self.po_number = "I" + ("0"*digits) + self.id.to_s
+      self.save
+    end
+  end
+
 end
