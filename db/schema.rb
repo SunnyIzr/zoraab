962b1e6dc64f66734ee0c6cd05ac7907742e89d7
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-02-20 17:58:22 -0500

    adding invoices table, model and spec

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140217190253) do
+ActiveRecord::Schema.define(version: 20140220225117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "batches", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "invoices", force: true do |t|
+    t.string   "po_number"
+    t.string   "vendor"
+    t.float    "total"
+    t.float    "shipping"
+    t.float    "discount"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
