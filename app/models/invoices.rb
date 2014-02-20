00bd167385dca76f74b2fe6ad806cf20ae92a155
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-02-20 18:21:37 -0500

    establishing polymorphic association between lineitems and orders/invoices

--- app/models/invoices.rb
+++ app/models/invoices.rb
@@ -1,2 +1,3 @@
 class Invoice < ActiveRecord::Base
+  has_many :line_items, as: :line_itemable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140220225117) do
+ActiveRecord::Schema.define(version: 20140220230727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20140220225117) do
     t.integer  "q"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "line_itemable_id"
+    t.string   "line_itemable_type", default: "Order"
   end
 
   create_table "orders", force: true do |t|
