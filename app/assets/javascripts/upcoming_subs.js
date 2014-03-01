54f7c64730f891634ffc5a2e675e0710b360ca39
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-01 11:52:16 -0500

    persisting completion state in upcoming subs model object

--- app/assets/javascripts/upcoming_subs.js
+++ app/assets/javascripts/upcoming_subs.js
@@ -1,15 +1,24 @@
 var UpcomingSubsController = {
   init: function() {
+
   },
 }
 
 var UpcomingSubsModel = {
+  completedRequest: false,
   getUpcomingSubs: function() {
     path = '/render-upcoming-subs/'
     $.get(path, function(data) {
       UpcomingSubsView.showUpcomingSubs(data)
+      UpcomingSubsModel.checkRequestComplete(data)
     });
+  },
+  checkRequestComplete: function(data) {
+    if ($.isEmptyObject(data) == false) {
+      UpcomingSubsModel.completedRequest = true
+    }
   }
+
 }
 var UpcomingSubsView = {
   showUpcomingSubs: function(data) {
