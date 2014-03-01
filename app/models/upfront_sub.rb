f118f3c4f25efd99ec2d8a5a65ad0809f794a50f
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-02-28 19:33:31 -0500

    adding a term for upfront subs

--- app/models/upfront_sub.rb
+++ app/models/upfront_sub.rb
@@ -1,2 +1,26 @@
 class UpfrontSub < Sub
+  def active?
+    self.sub_orders.size < self.term
+  end
+
+  def next_due_date
+    self.sub_orders.last.created_at + 30.days if self.active?
+  end
+
+  def due?
+    self.next_due_date < Time.new
+  end
+
+  def self.refresh
+    all.each do |usub|
+      if usub.due?
+        data = ChargifyResponse.parse(usub.chargify)
+        installment = usub.sub_orders.size + 1
+        trans_id = usub.cid.to_s + installment.to_s
+        oren = OutstandingRenewal.create(trans_id: trans_id.to_i, name: data[:name], plan: data[:plan], cid: usub.cid, amount: 0.0)
+        oren.created_at = usub.next_due_date
+        oren.save
+      end
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140228234537) do
+ActiveRecord::Schema.define(version: 20140301002921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -150,6 +150,7 @@ ActiveRecord::Schema.define(version: 20140228234537) do
     t.datetime "updated_at"
     t.boolean  "upfront",    default: false
     t.string   "type"
+    t.integer  "term"
   end
 
 end
