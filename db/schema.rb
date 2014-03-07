965825a00af5a5c134faf5c392663685db6dee62
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-07 17:05:19 -0500

    drop question mark in field name, was casuing issues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140307000419) do
+ActiveRecord::Schema.define(version: 20140307003558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,7 +85,7 @@ ActiveRecord::Schema.define(version: 20140307000419) do
     t.float    "shipping_charge",  default: 0.0
     t.float    "discount",         default: 0.0
     t.float    "fees",             default: 0.0
-    t.boolean  "post_to_shopify?", default: false
+    t.boolean  "post_to_shopify",  default: false
   end
 
   add_index "orders", ["batch_id"], name: "index_orders_on_batch_id", using: :btree
