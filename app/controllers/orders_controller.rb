723529b58d0d9812b7a3e1561793f671d97097a2
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-16 19:24:45 -0400

    puttingback method to remove order from datasession upon creation, since this will still be need at single order creation level

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -23,6 +23,7 @@ class OrdersController < ApplicationController
       @order.send_to_shopify if params[:commit] == "Save and Update Shopify" || params[:update_shopify] == '1'
       OutstandingSignup.refresh_outstanding_signups
       OutstandingRenewal.refresh_outstanding_renewals
+      DataSession.last.remove_order_due(@order)
       redirect_to order_path(@order.id)
     else
       render text: "FAIL!"
