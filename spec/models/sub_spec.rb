12495e5c7f0c145ff259c992eb013d3c89cd7e39
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-04-03 10:57:14 -0400

    cleaning up tests for sub model

--- spec/models/sub_spec.rb
+++ spec/models/sub_spec.rb
@@ -12,6 +12,7 @@ describe Sub do
   let (:pref1) {FactoryGirl.create(:pref)}
   let (:pref2) {FactoryGirl.create(:pref)}
   let (:pref3) {FactoryGirl.create(:pref)}
+  let (:pref4) {FactoryGirl.create(:pref)}
   let (:order1) {FactoryGirl.create(:sub_order)}
   let (:order2) {FactoryGirl.create(:sub_order)}
   let (:product1) {FactoryGirl.create(:product)}
@@ -25,6 +26,62 @@ describe Sub do
   it {should have_many (:sub_orders)}
   it {should validate_uniqueness_of(:cid) }
 
+  it 'should obtain Chargify response based on cid that contains all pertinent information' do
+      hash = sub.chargify
+      expect(hash[:id]).to eq(4258861)
+      expect(hash[:name]).to eq('SunnyShip IsraniShip')
+      expect(hash[:email]).to eq('sunny@zoraab.com')
+      expect(hash[:plan]).to eq('Sock Dabbler (2 Pairs/Mo)')
+      expect(hash[:price]).to eq(20)
+      expect(hash[:items]).to eq(2)
+      expect(hash[:status]).to eq('canceled')
+      expect(hash[:start_date]).to eq('25 Nov 2013')
+      expect(hash[:shipping_address]).to eq({
+            :name => 'SunnyShip IsraniShip',
+            :address => '123 Shipping Street',
+            :address2 => nil,
+            :city => 'Ship City',
+            :state => 'ON',
+            :zip => '12345',
+            :country => 'CA',
+            :phone => nil
+          })
+      expect(hash[:billing_address]).to eq({
+            :name => 'Sunny Israni',
+            :address => '43 Rosenbrook Drive',
+            :address2 => nil,
+            :city => 'Lincoln Park',
+            :state => 'NJ',
+            :zip => '07035',
+            :country => 'US',
+            :phone => nil})
+    end
+    
+  it "should retrieve Wufoo preferences" do
+    pref1.save
+    pref2.pref = 'casual'
+    pref2.save
+    pref3.pref = 'fun'
+    pref3.save
+
+    expect(sub.retrieve_wufoo_prefs). to eq([pref1,pref3])
+
+  end
+  
+  it 'should list prefs as string' do
+    pref1.save
+    pref2.pref = 'casual'
+    pref2.save
+    pref3.pref = 'fun'
+    pref3.save
+    pref4.pref = 'fashion'
+    pref4.save
+    sub1.prefs << [pref1,pref2,pref3,pref4]
+    sub1.save
+    
+    expect(sub1.list_prefs).to eq('BCWF')
+  end
+    
   it "should provide a list of previously purchased SKUs" do
     sub.save
     product1.sku = 'sku_1'
@@ -52,57 +109,46 @@ describe Sub do
     expect(sub.order_history).to eq(['sku_3', 'sku_4', 'sku_1', 'sku_2'])
 
   end
-
-  it "should retrieve Wufoo preferences" do
-    pref1.save
-    pref2.pref = 'casual'
-    pref2.save
-    pref3.pref = 'fun'
-    pref3.save
-
-    expect(sub.retrieve_wufoo_prefs). to eq([pref1,pref3])
-
-  end
-
-  it 'should obtain Chargify response based on cid that contains all pertinent information' do
-    expect(sub.chargify['state']).to eq('canceled')
-    expect(sub.chargify['customer'].first_name).to eq('SunnyShip')
-    expect(sub.chargify['customer'].last_name).to eq('IsraniShip')
-    expect(sub.chargify['customer'].email).to eq('sunny@zoraab.com')
-    expect(sub.chargify['product'].name).to eq('Sock Dabbler (2 Pairs/Mo)')
-
-  end
-
-  it 'should indicate whether an order for the next pmt date was NOT created' do
+  
+  it 'should indicate whether a given sku has already been sent to sub' do
     sub.save
-    next_pmt_date = ChargifyResponse.next_pmt_date(sub.chargify)
-    order.created_at = next_pmt_date
-    order.save
-    sub.sub_orders << order
-    expect(sub.not_exist?(next_pmt_date)).to eq(false)
+    product1.sku = 'sku_1'
+    product1.save
+    product2.sku = 'sku_2'
+    product2.save
+    product3.sku = 'sku_3'
+    product3.save
+    product4.sku = 'sku_4'
+    product4.save
+    product5.sku = 'sku_5'
+    product5.save
+    
+    order1.line_items.create(product_id: product1.id, q: 1, rate: 0.0)
+    order1.line_items.create(product_id: product2.id, q: 1, rate: 0.0)
+
+    order2.line_items.create(product_id: product3.id, q: 1, rate: 0.0)
+    order2.line_items.create(product_id: product4.id, q: 1, rate: 0.0)
+    
+    sub.sub_orders << [order1,order2]
+    
+    expect(sub.dupe?('sku_5')).to eq(false)
+    expect(sub.dupe?('sku_4')).to eq(true)
   end
 
-  it "should pull back all subs that are due within a time period" do
-    subs = Sub.retrieve_all_active_subs
-    subs_due = []
-    subs_not_due = []
-    subs.each { |key,value| subs_due << key if value[:next_pmt_date] < (Time.new + (5.day)) }
-    subs.each { |key,value| subs_not_due << key if value[:next_pmt_date] > (Time.new + (5.day)) }
-    sub1.cid = subs_due[0]
-    sub1.save
-    sub2.cid = subs_due[1]
-    sub2.save
-    sub3.cid = subs_due[2]
-    sub3.save
-    sub4.cid = subs_not_due[0]
-    sub4.save
-    sub5.cid = subs_not_due[1]
-    sub5.save
-    sub6.cid = subs_not_due[2]
-    sub6.save
-    expect(Sub.pull_subs_due(5)).to eq({sub1.cid => subs[sub1.cid], sub2.cid => subs[sub2.cid], sub3.cid => subs[sub3.cid]})
+  it 'should indicate whether a future order was already created given an order date' do
+    sub.save
+    product1.sku = 'sku_1'
+    product1.save
+    product2.sku = 'sku_2'
+    product2.save
+    order1.line_items.create(product_id: product1.id, q: 1, rate: 0.0)
+    order1.line_items.create(product_id: product2.id, q: 1, rate: 0.0)
+    order1.created_at = Time.new(2014,1,1)
+    order1.sub = sub
+    order1.save
+    
+    expect(sub.order_already_created?(Time.new)).to eq(false)
+    expect(sub.order_already_created?(Time.new(2014,1,1))).to eq(true)
   end
-
-
-
+  
 end
