bdc02ab2d583bf5f3f7ec74fac46995b5576e41b
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-04-05 19:59:49 -0400

    adding index action

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -2,4 +2,7 @@ class InvoicesController < ApplicationController
   def show
     @invoice = Invoice.find(params[:id])
   end
+  def index
+    @invoices = Invoice.paginate(:page => params[:page], :order => 'created_at DESC', :per_page => 30)
+  end
 end
