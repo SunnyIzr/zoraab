bc58df2023cb1f091609dd32b9f39fa70fe2d51e
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-04-05 22:22:50 -0400

    creating a vendor model (non AR) to hold all vendor data

---
+++ app/models/vendor.rb
@@ -0,0 +1,16 @@
+class Vendor
+  def self.all
+    [
+      'Foot Traffic',
+      'K.Bell',
+      'Ozone',
+      'Pact',
+      'Perry Ellis',
+      'Renfro',
+      'Richer Poorer',
+      'Sock it to Me',
+      'United Legwear',
+      'Unsimply Stitched'
+    ]
+  end
+end
