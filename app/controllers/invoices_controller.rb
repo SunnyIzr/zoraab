b447a75c7131508ffc28ea4063cb587ced8af3f4
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-04-05 22:22:09 -0400

    methods to create a new invoice with line items

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,8 +1,38 @@
 class InvoicesController < ApplicationController
+
   def show
     @invoice = Invoice.find(params[:id])
   end
+
   def index
     @invoices = Invoice.paginate(:page => params[:page], :order => 'created_at DESC', :per_page => 30)
+  end
+
+  def new
+    @invoice = Invoice.new
+    3.times { @invoice.line_items.build }
+  end
+  
+  def create
+    p '*'*100
+    p params
+    skus = params[:skus]
+    rates = params[:rates]
+    qs = params[:qs]
+    @invoice = Invoice.new(vendor: params[:invoice][:vendor],created_at: params[:invoice][:created_at])
+    if @invoice.save
+      @invoice.set_line_items(skus,rates,qs)
+      redirect_to invoice_path(@invoice)
+    else
+      render text: 'fail!'
+    end
+    
+    
+    
+  end
+  
+  private
+  def invoice_params
+    params.permit(:invoice).permit(:created_at,:vendor)
   end
 end
