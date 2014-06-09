d1b60cab1f5c69ccc8295657eebca6ce009c7ed7
Sunny Israni <sunny@zoraab.com>
2014-06-09 14:56:54 -0400

    updating new shipstation api url

--- config/initializers/shipstation.rb
+++ config/initializers/shipstation.rb
@@ -1,1 +1,1 @@
-SHIPSTATION = OData::Service.new 'https://data.shipstation.com/1.3/', {:username => ENV['SHIP_LOGIN'], :password => ENV['SHIP_PW'], :namespace => 'SHIP'}
+SHIPSTATION = OData::Service.new 'https://data3.shipstation.com/1.3/', {:username => ENV['SHIP_LOGIN'], :password => ENV['SHIP_PW'], :namespace => 'SHIP'}
