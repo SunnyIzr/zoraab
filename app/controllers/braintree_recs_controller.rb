7c9c416255b3472f1507878328f4f3c3b6f73585
Sunny Israni <sunny@zoraab.com>
2014-09-04 12:56:33 -0400

    controller methods to: * display upload view which will have a form to create a new braintree rec * create a new braintree rec by feeding in csv file to parse and persist * display disb-rec view which will show an in progress rec

--- app/controllers/braintree_recs_controller.rb
+++ app/controllers/braintree_recs_controller.rb
@@ -1,4 +1,20 @@
 class BraintreeRecsController < ApplicationController
   def disb_rec
+    @braintree_rec = BraintreeRec.find(params[:id])
   end
+  def upload_braintree
+    @braintree_rec = BraintreeRec.new
+  end
+  def create
+    file = params[:braintree_rec][:transactions]
+    @braintree_rec = BraintreeRec.create(rec_date: params[:braintree_rec][:rec_date])
+    @braintree_rec.import_csv(file.path)
+    @braintree_rec.group_transactions
+    redirect_to disb_rec_path(@braintree_rec)
+  end
+  private
+  def braintree_rec_params
+    params.require(:braintree_rec).permit(:rec_date,:transactions)
+  end
+  
 end
