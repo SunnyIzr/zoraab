44c689c44df2c3f5030c396ff0d4a71a1a6eb01d
Sunny Israni <sunny@zoraab.com>
2014-10-11 15:01:34 -0400

    adding headers for cors

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,5 +1,7 @@
 class SubsController < ApplicationController
   skip_before_filter  :verify_authenticity_token, :only => :shopify_data
+  before_filter :cors_preflight_check, :only => :shopify_data
+  after_filter :cors_set_access_control_headers, :only => :shopify_data
   def new
     @sub = Sub.new
   end
@@ -94,6 +96,25 @@ class SubsController < ApplicationController
     @sub = Sub.find_by(shopify_id: params[:shopify_id])
     data = {sub: @sub, cid: @sub.chargify, orders: @sub.sub_orders}
     render json: data
+  end
+  
+  # For all responses in this controller, return the CORS access control headers.
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  # If this is a preflight OPTIONS request, then short-circuit the
+  # request, return only the necessary headers and return an empty
+  # text/plain.
+
+  def cors_preflight_check
+    p '~'*100
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+    headers['Access-Control-Max-Age'] = '1728000'
   end
 
   private
